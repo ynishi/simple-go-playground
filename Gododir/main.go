df6da9e6f9b1dd8c8613400fce0bbef0bad8e0a0
Yutaka Nishimura <ytk.nishimura@gmail.com>
2017-12-24 15:37:53 +0900

    update main.go

--- Gododir/main.go
+++ Gododir/main.go
@@ -24,9 +24,85 @@ func main() {
 }
 `
 
+func getLatestFileNum(prj string) (max int) {
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rprj := regexp.MustCompile(fmt.Sprintf("%s.[0-9]+.go$", prj))
+	rnum := regexp.MustCompile(".[0-9]+.go$")
+	max = 0
+	for _, file := range files {
+		if rprj.MatchString(file.Name()) {
+			s := rnum.FindString(file.Name())
+			i, err := strconv.Atoi(s[1 : len(s)-3])
+			if err != nil {
+				log.Fatalf("Cannot parse file num: %s", err)
+			}
+			if max < i {
+				max = i
+			}
+		}
+	}
+	return max
+}
+
+func getLatestFileName(prj string) (filename string) {
+	max := getLatestFileNum(prj)
+	filename = fmt.Sprintf("%s.%d.go", prj, max)
+	_, err := os.Stat(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return filename
+}
+
+func genNewFileName(prj string) (filename string) {
+	filename = fmt.Sprintf("%s.go", prj)
+	if _, err := os.Stat(fmt.Sprintf("%s.go", prj)); err == nil {
+		max := getLatestFileNum(prj)
+		filename = fmt.Sprintf("%s.%d.go", prj, max+1)
+	}
+	return filename
+}
+
+func genPrjName(defaultPrj string, c *do.Context) (prj string) {
+	prj = defaultPrj
+	if len(c.Args.NonFlags()) == 1 {
+		name := c.Args.NonFlags()[0]
+		if path.Ext(name) == ".go" {
+			prj = name[0 : len(name)-3]
+		} else {
+			prj = name
+		}
+	}
+	return prj
+}
+
+const DEFAULT_PRJ_NAME = "play"
+
 func tasks(p *do.Project) {
 
 	p.Task("default", do.S{"play"}, nil)
+
+	p.Task("run", nil, func(c *do.Context) {
+		prj := genPrjName(DEFAULT_PRJ_NAME, c)
+		var filename string
+		if len(c.Args.NonFlags()) == 1 {
+			name := c.Args.NonFlags()[0]
+			if path.Ext(name) == ".go" {
+				filename = name
+			} else {
+				filename = getLatestFileName(prj)
+			}
+		} else {
+			filename = getLatestFileName(prj)
+		}
+		fmt.Printf("Run: %s\n", filename)
+		//force goimports
+		c.Run(fmt.Sprintf("goimports -w %s", filename))
+		c.Run(fmt.Sprintf("go run %s", filename))
+	})
 
 	p.Task("play", nil, func(c *do.Context) {
 		if c.FileEvent != nil {
@@ -39,39 +115,10 @@ func tasks(p *do.Project) {
 
 	p.Task("new", nil, func(c *do.Context) {
 		// defalut is play
-		prj := "play"
-		if len(c.Args.NonFlags()) == 1 {
-			name := c.Args.NonFlags()[0]
-			if path.Ext(name) != "go" {
-				prj = name
-			} else {
-				prj = name[0 : len(name)-3]
-			}
-		}
+		prj := genPrjName(DEFAULT_PRJ_NAME, c)
+
 		// defalut is {{prj}].go
-		filename := fmt.Sprintf("%s.go", prj)
-		if _, err := os.Stat(fmt.Sprintf("%s.go", prj)); err == nil {
-			files, err := ioutil.ReadDir(".")
-			if err != nil {
-				log.Fatal(err)
-			}
-			rprj := regexp.MustCompile(fmt.Sprintf("%s.[0-9]+.go$", prj))
-			rnum := regexp.MustCompile(".[0-9]+.go$")
-			max := 0
-			for _, file := range files {
-				if rprj.MatchString(file.Name()) {
-					s := rnum.FindString(file.Name())
-					i, err := strconv.Atoi(s[1 : len(s)-3])
-					if err != nil {
-						log.Fatalf("Cannot parse file num: %s", err)
-					}
-					if max < i {
-						max = i
-					}
-				}
-			}
-			filename = fmt.Sprintf("%s.%d.go", prj, max+1)
-		}
+		filename := genNewFileName(prj)
 		fmt.Printf("Create: %s\n", filename)
 		file, err := os.Create(filename)
 		if err != nil {
